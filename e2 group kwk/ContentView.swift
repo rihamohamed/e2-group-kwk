cfad0b1117554b842f0f0795532f96135f320707
Diya Sundhar <diyasundhar4@gmail.com>
2023-06-29 17:59:10 -0700

    submit name

--- e2 group kwk/ContentView.swift
+++ e2 group kwk/ContentView.swift
@@ -1,23 +1,48 @@
 //
 //  ContentView.swift
-//  e2 group kwk
+//  e-2 group
 //
-//  Created by Riham Mohamed on 6/29/23.
+//  Created by Riham Mohamed on 6/26/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var textTile = "Enter Name"
     var body: some View {
+        
+    
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hi, world!")
-        }
-        .padding()
-    }
+            Text(textTile)
+            TextField("", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 3)
+                .padding()
+          
+            Button {
+                textTile = "Welcome to EcoGuide, \(name)!"
+
+            } label: {
+                Image("SubmitName").resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                
+               
+                    
+           
+                }
+
+                    
+                }
+                    }
+    
+        
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
